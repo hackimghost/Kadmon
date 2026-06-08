a1d15248f0f462968b0429e1ca2323f009ee39a3
Phantom Securyt System <188284770+hackimghost@users.noreply.github.com>
2026-06-08 19:21:00 -0300

    Add restricted access comments to low_level_mutator.zig

--- low_level_mutator.zig
+++ low_level_mutator.zig
@@ -1,1 +1,16 @@
 // Código-fonte retido sob protocolo de Segurança Operacional (OPSEC).
+// Código-fonte retido sob protocolo de Segurança Operacional (OPSEC).
+// [ RESTRICTED ACCESS ] - Zero-Overhead Memory Mutator
+// [ RESTRICTED ACCESS ] - Direct Syscall Invocation Table
+// [ RESTRICTED ACCESS ] - NTAPI Hooking & Unhooking
+// [ RESTRICTED ACCESS ] - Fileless RAM Injection Matrix
+// [ RESTRICTED ACCESS ] - Process Hollowing Architecture
+// [ RESTRICTED ACCESS ] - Reflective DLL Loading Logic
+// [ RESTRICTED ACCESS ] - Arbitrary Code Execution Block
+// [ RESTRICTED ACCESS ] - Privilege Escalation Vectors
+// [ RESTRICTED ACCESS ] - Heap Manipulation and Spraying
+// [ RESTRICTED ACCESS ] - Undocumented Windows APIs
+// [ RESTRICTED ACCESS ] - Linux Kernel Module Rootkit Hooks
+// [ RESTRICTED ACCESS ] - Memory Protections Bypass (RWX)
+// [ RESTRICTED ACCESS ] - Polymorphic Shellcode Generator
+// [ RESTRICTED ACCESS ] - Stealth Execution Subsystem
